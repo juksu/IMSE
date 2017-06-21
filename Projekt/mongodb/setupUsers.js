4ec0b4322b299f0c5a64d77ef7bf7f2c608f37c1
Andreas Fuchs <a1106307@unet.univie.ac.at>
2017-06-21 14:02:56 +0200

    bugfix

--- Projekt/mongodb/setupUsers.js
+++ Projekt/mongodb/setupUsers.js
@@ -1,11 +1,11 @@
 // open mongo shell with database owner/admin 
-	// (you need to have one specified by :
+	// if you have already set auth = true (conf file) then you need to be dbOwner, dbAdmin or dbUserAdmin
+	// if you want to specify onw manually:
 	// db.createUser({ user:"<owner or admin name>", pwd:"<password>", roles:["dbOwner"] })
-// mongo -u <owner or admin> -p
-// then load the script with
+	// mongo eebf -u <username> -p
+// load the script with
 // load("<file location>")
 
-use eebf;
 db.createUser({ user:"eeBF_Admin", pwd:"Tombstone", roles:["dbAdmin"] });
 db.createUser({ user:"eeBF_Kunde", pwd:"Silverado", roles:["readWrite"] });
 db.createUser({ user:"eeBF_Benutzer", pwd:"Unforgiven", roles:["read"] });
